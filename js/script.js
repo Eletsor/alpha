8f4b0e9ce9f12cbdaf5e0c8f40f123564166bcd9
Mikhail Arhipov <arkhipoff.misha2015@gmail.com>
2024-08-31 15:01:52 +0300

    modal

--- js/script.js
+++ js/script.js
@@ -3,4 +3,45 @@ menutoggle.onclick = function(){
   menutoggle.classList.toggle('active')
  } 
 
+ var modal = document.getElementById('myModal');
+ var btn = document.getElementById("myBtn");
+ var span = document.getElementsByClassName("close")[0];
+ btn.onclick = function() {
+  modal.style.display = "block";
+  document.getElementsByTagName("body")[0].style.overflow = 'hidden';
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.getElementsByTagName("body")[0].style.overflow = 'scroll';
+  }
+}
+
+var modal = document.getElementById('myModaly');
+ var btn = document.getElementById("myBtny");
+ var span = document.getElementsByClassName("close")[0];
+ btn.onclick = function() {
+  modal.style.display = "block";
+  document.getElementsByTagName("body")[0].style.overflow = 'hidden';
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.getElementsByTagName("body")[0].style.overflow = 'scroll';
+  }
+}
+
+var modal = document.getElementById('myModalx');
+ var btn = document.getElementById("myBtnx");
+ var span = document.getElementsByClassName("close")[0];
+ btn.onclick = function() {
+  modal.style.display = "block";
+  document.getElementsByTagName("body")[0].style.overflow = 'hidden';
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.getElementsByTagName("body")[0].style.overflow = 'scroll';
+  }
+}
 
